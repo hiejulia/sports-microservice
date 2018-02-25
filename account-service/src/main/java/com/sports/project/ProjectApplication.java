2ee0b8452a873fcecd1397c52fc8322fa942c0e3
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-25 22:18:54 +0200

    update account service

--- account-service/src/main/java/com/sports/project/ProjectApplication.java
+++ account-service/src/main/java/com/sports/project/ProjectApplication.java
@@ -2,12 +2,8 @@ package com.sports.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbineStream
-@EnableHystrixDashboard
 public class ProjectApplication {
 
 	public static void main(String[] args) {
